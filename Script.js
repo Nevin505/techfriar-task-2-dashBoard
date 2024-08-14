893116e32d64d79e173d410b07db7ae0174b804a
Nevin <nevinjohn63@gmail.com>
2024-08-14 23:43:32 +0530

    made some dimension changes for footer

--- Script.js
+++ Script.js
@@ -1,8 +1,12 @@
 // to change the text Dynamically
 const complainceNumber=document.getElementById('complainceNumber').textContent=4;
+const values = [12, 6   , 10]; // Your input values
+
+const colors = ['#f39c6b', '#e74c3c', '#27ae60']; // Colors for each segment
 
 function createDoughnutChart(values, colors, gap = 2) {
     const total = values.reduce((acc, val) => acc + val, 0);
+    const AppicationNumber=document.getElementById('doughnut-chart-text').innerText=`${total} Applications`
     let cumulativePercent = 0;
     const gapPercent = gap / 360; // convert gap to percentage of the circle
 
@@ -13,7 +17,7 @@ function createDoughnutChart(values, colors, gap = 2) {
         const endPercent = cumulativePercent - gapPercent;
 
         // Return the gradient string for this segment, with a small gap
-        return `${colors[index]} ${startPercent * 100}% ${endPercent * 100}%, transparent ${0.12 * 100}%`;
+        return `${colors[index]} ${startPercent * 100}% ${endPercent * 100}%, transparent ${0.20 * 100}%`;
     }).join(', ');
 
     // Set the conic-gradient dynamically with gaps
@@ -23,19 +27,16 @@ function createDoughnutChart(values, colors, gap = 2) {
     // document.getElementById('chartValue').textContent = total;
 }
 
-// Example Usage
-const values = [20, 6, 10]; // Your input values
-const colors = ['#f39c6b', '#e74c3c', '#27ae60']; // Colors for each segment
-createDoughnutChart(values, colors, 2); // 2 degrees gap between each segment
+
+createDoughnutChart(values, colors, 3); // 2 degrees gap between each segment
 
 
 // Select Clickable Links
 
 const navLinkItems=document.getElementsByClassName('nav-link-items');
 
-console.log(navLinkItems);
 
-
+// To Remove and add the background effect when a navLinks is being clicked
 for(let navLinkItem of navLinkItems){
     navLinkItem.addEventListener('click',()=>{
          for(let navItems of navLinkItems){
@@ -45,3 +46,17 @@ for(let navLinkItem of navLinkItems){
     })
 }
 
+
+// To give the color
+// application-status-content
+const listItems=document.getElementsByClassName('color-indicator');
+
+
+let item=0;
+for(let listItem of listItems){
+    item++;
+    listItem.style.backgroundColor = colors[item];
+
+}
+// listItems.forEach((indicator, index) => {
+// });
